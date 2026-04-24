53cd63ddfb0533444b20934ec7c1baa5d2b2956b
长生 <jacke.yang@uccvr.com>
2026-04-24 17:44:22 +0800

    临时更新

--- Big-A-Stock-Calculator/App.xaml.cs
+++ Big-A-Stock-Calculator/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Serilog;
 
 namespace Big_A_Stock_Calculator
 {
@@ -9,6 +10,24 @@ namespace Big_A_Stock_Calculator
     /// </summary>
     public partial class App : Application
     {
-    }
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Async(a => a.File("logs/app-.log", rollingInterval: RollingInterval.Day))
+                .WriteTo.Console()
+                .CreateLogger();
 
+            Log.Information("Application Starting...");
+
+            base.OnStartup(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            Log.Information("Application Exiting...");
+            Log.CloseAndFlush();
+            base.OnExit(e);
+        }
+    }
 }
